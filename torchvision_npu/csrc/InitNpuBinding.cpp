0d569e150ee9d8f6aa10f318c123d827b9f89100
Fang Xiaolong <xiaoloongfang@gmail.com>
2022-12-16 11:53:48 +0800

    单文件能跑通版本

--- torchvision_npu/csrc/InitNpuBinding.cpp
+++ torchvision_npu/csrc/InitNpuBinding.cpp
@@ -4,6 +4,7 @@
 #include <torch/csrc/utils/python_arg_parser.h>
 #include <torch/csrc/Exceptions.h>
 #include <torch/library.h>
+
 #include <ATen/ATen.h>
 
 PyObject* module;
@@ -24,19 +25,49 @@ void AddPyMethodDefs(std::vector<PyMethodDef>& vector, PyMethodDef* methods)
   }
 }
 
-static std::vector<PyMethodDef> methods;
+PyObject * FirstFunction(PyObject * /* unused */)
+{
+  fprintf(stdout, "call c function success.\n");
+  Py_RETURN_NONE;
+}
+
+//PyObject * TensorAdd(PyObject* self_, PyObject* args, PyObject* kwargs)
+//{
+//  fprintf(stdout, "call c function success.\n");
+//  Py_RETURN_NONE;
+//}
+
+static PyObject * TensorAdd(PyObject* self_, PyObject* args, PyObject* kwargs)
+{
+  HANDLE_TH_ERRORS
+  static torch::PythonArgParser parser({
+    "tensor_add(Tensor a, Tensor b)",
+  }, /*traceable=*/true);
+  torch::ParsedArgs<2> parsed_args;
+  auto _r = parser.parse(nullptr, args, kwargs, parsed_args);
+
+  return torch::autograd::utils::wrap(_r.tensor(0).add(_r.tensor(1)));
+
+//  Py_RETURN_NONE;
+  END_HANDLE_TH_ERRORS
+}
+
+
+static PyMethodDef TorchNpuMethods[] = {
+  {"FirstFunction", (PyCFunction)FirstFunction, METH_NOARGS, nullptr},
+  {nullptr, nullptr, 0, nullptr}
+};
 
 static PyMethodDef TorchNpuOps[] = {
   {"tensor_add", (PyCFunction)TensorAdd, METH_VARARGS | METH_KEYWORDS, nullptr},
   {nullptr, nullptr, 0, nullptr}
 };
 
-PyMethodDef* getTorchNpuOps(){
-    return TorchNpuOps;
-}
+static std::vector<PyMethodDef> methods;
 
 PyObject* initMoudle(){
-  AddPyMethodDefs(methods, getTorchNpuOps());
+  AddPyMethodDefs(methods, TorchNpuMethods);
+  AddPyMethodDefs(methods, TorchNpuOps);
   static struct PyModuleDef torchnpu_module = {
      PyModuleDef_HEAD_INIT,
      "torchvision_npu.ops",
